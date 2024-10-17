0a3de2a0ff8723b5e348a18044e551da33e62ce7
timosdev99 <timothyisah4@gmail.com>
2024-10-17 15:27:00 +0100

    added features

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 import inquirer from 'inquirer';
 import dotenv from 'dotenv';
 import path from 'path';
+import fs from'fs'
 
 
 
@@ -49,19 +50,70 @@ const generateProjectIdea = async (input) => {
   }
 };
 
+const projectswork = {
+  Creative_Art: "Creative_Art",
+  complex: "complex",
+  skill_challenge: "skill_challenge",
+  self_development:  "self_development",
+  others: "others"
+}
+  
+
 const promptUserForCategory = async () => {
   try {
+    
+    const { List } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'List',
+        message: 'What kind of project are you in the mood for:',
+        choices: Object.keys(projectswork)
+      },
+    ]);
+    
+    let idea;
+
+   if(List=== "others") {
     const { category } = await inquirer.prompt([
       {
         type: 'input',
         name: 'category',
-        message: 'What kind of project are you in the mood for?',
+        message: 'input What kind of project are you in the mood for:',
         validate: isRequired,
       },
     ]);
 
-    const idea = await generateProjectIdea(category);
+     idea = await generateProjectIdea(category);
     console.log(`\nHere's a project idea for you:\n${idea}`);
+
+   }
+   else {
+     idea = await generateProjectIdea(List);
+    console.log(`\nHere's a project idea for you:\n${idea}`);
+   }
+
+   const { Save } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'Save',
+      message: 'do you want to save project idea:',
+      choices: ["yes", "no"]
+    },
+  ]);
+    
+
+  if(Save === "yes") {
+    fs.writeFile("./bin/favorite.json", JSON.stringify(idea), 'utf-8', (err) => {
+      if (err) {
+        console.error('An error occurred:', err);
+      } else {
+        console.log('Idea saved successfully!');
+      }
+    });
+  }
+
+  
+    
   } catch (error) {
     console.error('An error occurred:', error.message);
   }
